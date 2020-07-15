62a28e334c4df3ebdc727a363eada8b089e3d64b
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-07-15 15:22:14 +0100

    Correct permissions for created file

--- Components/Scripts/set-user-icon.sh
+++ Components/Scripts/set-user-icon.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ../Misc
-sudo cp UserIcon.png "/var/lib/AccountsService/icons/$USER"
-sudo sed "s|.*Icon=.*|Icon=/var/lib/AccountsService/icons/$USER|" "user-AccountsService" | sudo tee "/var/lib/AccountsService/users/$USER" > /dev/null
+if [[ "$USER" != "root" ]]; then
+  sudo cp UserIcon.png "/var/lib/AccountsService/icons/$USER"
+  sudo sed "s|.*Icon=.*|Icon=/var/lib/AccountsService/icons/$USER|" "user-AccountsService" | sudo tee "/var/lib/AccountsService/users/$USER" > /dev/null
+  sudo chmod 600 "/var/lib/AccountsService/users/$USER"
+fi
