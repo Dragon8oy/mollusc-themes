cab44b140d03093187835e7eef37318cd331a9c4
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-07-28 21:15:38 +0100

    Updated to work with any filename in Wallpapers/

--- Components/Scripts/set-wallpaper.sh
+++ Components/Scripts/set-wallpaper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cd "$(dirname "${BASH_SOURCE[0]}")"
-cd ../Wallpapers
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+cd ../Wallpapers || exit 1
 
 mkdir -p ~/Pictures/Wallpapers/
-cp *.png ~/Pictures/Wallpapers/
+cp ./*.png ~/Pictures/Wallpapers/
 
-gsettings set org.gnome.desktop.background picture-uri 'file:///home/ratus5/Pictures/Wallpapers/NightMountains.png'
+wallpaper="$(ls ./*.png)"
+gsettings set org.gnome.desktop.background picture-uri "file:///home/$USER/Pictures/Wallpapers/$wallpaper"
 gsettings set org.gnome.desktop.background picture-options 'zoom'
