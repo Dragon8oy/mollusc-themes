0730f7501a2519f49158a4cacae16f73dcbb50e9
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-08-22 11:41:46 +0100

    Added script to fix tearing and enable freesync on my monitors

---
+++ Components/Scripts/fix-tearing-freesync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp ../Misc/99-force-composition-pipeline-freesync /etc/X11/xinit/xinitrc.d/99-force-composition-pipeline-freesync
+sudo chmod +x /etc/X11/xinit/xinitrc.d/99-force-composition-pipeline-freesync
