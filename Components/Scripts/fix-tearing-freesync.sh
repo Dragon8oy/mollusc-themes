99b92c27d6e4550191e1c3a3d9ab80184345a21f
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-09-02 18:52:43 +0100

    Use config file instead of command to fix tearing with my monitor setup

--- Components/Scripts/fix-tearing-freesync.sh
+++ Components/Scripts/fix-tearing-freesync.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo cp ../Misc/99-force-composition-pipeline-freesync /etc/X11/xinit/xinitrc.d/99-force-composition-pipeline-freesync
-sudo chmod +x /etc/X11/xinit/xinitrc.d/99-force-composition-pipeline-freesync
+sudo cp ../Misc/force-composition-pipeline-freesync.conf /etc/X11/xorg.conf.d/force-composition-pipeline-freesync.conf
