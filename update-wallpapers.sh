a166367f086f60fda634e0ef6304ded20a972f66
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-10-24 15:18:47 +0100

    Remove Makefile from argon-wallpapers

--- update-wallpapers.sh
+++ update-wallpapers.sh
@@ -3,4 +3,4 @@ cd Components || exit 1
 rm -rf Wallpapers
 git clone https://github.com/Dragon8oy/argon-wallpapers.git
 mv argon-wallpapers Wallpapers
-rm -rf Wallpapers/README.md Wallpapers/compress-wallpapers.sh
+rm -rf Wallpapers/README.md Wallpapers/Makefile
