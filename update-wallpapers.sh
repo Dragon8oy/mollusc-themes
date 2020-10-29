4276f5ea1189bd0155a1075abb1ae7d6c19dc4cb
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-10-29 13:49:00 +0000

    Updated to match structure of wallpaper repo

--- update-wallpapers.sh
+++ update-wallpapers.sh
@@ -3,8 +3,9 @@ cd Components || exit 1
 rm -rf Wallpapers
 git clone https://github.com/Dragon8oy/argon-wallpapers.git
 mv argon-wallpapers Wallpapers
+rm -rf Wallpapers/docs
 for file in Wallpapers/*; do
-  if [[ "$file" != *".png" ]] || [[ "$file" == *"Banner.png"* ]]; then
+  if [[ "$file" != *".png" ]]; then
     rm -rf "./$file"
   fi
 done
